2a681ef249ee1137bda319313b3b0513282e5925
Ct <33597729+Ct17@users.noreply.github.com>
2019-07-05 21:34:35 +0800

    添加homepage页面的若干按钮，在app.js中添加openid的获取

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 //app.js
+
+var GetUseUrl = require('config.js').GetUseUrl;
+var GetOpenidUrl = require('config.js').GetOpenidUrl; 
+
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -9,6 +13,57 @@ App({
     // 登录
     wx.login({
       success: res => {
+        if(res.code){
+          console.log(res.code);
+          wx.showLoading({
+            title: '获取用户信息',
+          })
+
+          wx.request({
+            url: GetOpenidUrl,
+            data:{
+              js_code:res.code,
+            },
+            success:function(res){
+              wx.setStorage({
+                key: 'openid',
+                data: res.data,
+              })
+              console.log('openid  '+res.data);
+              wx.hideLoading();
+              /*wx.request({
+                url: GetUseUrl,
+                data:{
+                  openid: res.data
+                },
+                success:function(res){
+                  console.log(res.data);
+                  if(res.data===''){
+                    wx.navigateTo({
+                      url: '../bind/bind',
+                    })
+                  }else{
+                    if(res.data.admin==1){
+                      wx.setStorage({
+                        key: 'admin',
+                        data: true,
+                      })
+                    }else{
+                      if(res.data.admin ==0){
+                        wx.setStorage({
+                          key: 'admin',
+                          data: false,
+                        })
+                      }
+                    }
+                  }
+                }
+              })*/
+
+            }
+          })
+
+        }
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -20,8 +75,11 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
+              //this.globalData.userInfo = res.userInfo
+              wx.setStorage({
+                key: 'userInfo',
+                data: res.userInfo,
+              })
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
---
+++ config.js
@@ -0,0 +1,13 @@
+/**配置文件 */
+
+var host = "dlut-elab.com/hcq/dlut-bit"
+
+var config = {
+  host,
+  GetUseUrl:`http://${host}/getuse.php`,
+
+  GetOpenidUrl:`http://${host}/getopenid.php`,
+
+
+}
+module.exports = config;
